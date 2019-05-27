323bd92e9b3be65e7e37c781f3fa8df53a3fe356
Tanish Grover <tanigrover@gmail.com>
2019-05-27 23:29:27 +0530

    completed refactoring of colorbox styles

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -19,10 +19,7 @@ const styles = theme => ({
 		}
 	},
 	copyText: {
-		color: props => chroma(props.color).luminance() >= 0.7 ? 'black' : 'white',
-		'&h1': {
-			textShadow: props => chroma(props.color).luminance() >= 0.7 && '1px 2px black'
-		}
+		color: props => chroma(props.color).luminance() >= 0.7 ? 'black' : 'white',	
 	},
 	colorName: {
 		color: props => chroma(props.color).luminance() <= 0.085 ? 'rgba(240, 240, 240, 0.9)' : 'black'
@@ -58,6 +55,73 @@ const styles = theme => ({
 		border: 'none',
 		opacity: '0',
 		transition: 'opacity 0.5s ease',
+	},
+
+	boxContent: {
+		position: 'absolute',
+		padding: '10px',
+		width: '100%',
+		bottom: '0',
+		left: '0',
+		letterSpacing: '1px',
+		fontSize: '10px',
+	},
+
+	copyOverlay: {
+		position: 'fixed',
+		top: '0',
+		bottom: '0',
+		left: '0',
+		right: '0',
+		opacity: '0',
+		width: '100%',
+		height: '100%',
+		zIndex: '10',
+		transform: 'scale(0.1)',
+		transition: 'all 0.5s ease-in-out',
+
+		'&.show': {
+			opacity: '1',
+			transform: 'scale(10)',
+		}
+	},
+
+	copyMessage: {
+		position: 'fixed',
+		width: '100%',
+		left: '0',
+		top: '0',
+		right: '0',
+		bottom: '0',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		fontSize: '3.5rem',
+		opacity: '0',
+		transform: 'scale(0.01)',
+
+		'& h1': {
+			fontWeight: '400',
+			background: 'rgba(255, 255, 255, 0.2)',
+			width: '100%',
+			textShadow: props => chroma(props.color).luminance() <= 0.7 && '1px 2px black',
+			textAlign: 'center',
+			marginBottom: '0',
+			padding: '1rem',
+		},
+
+		'& p': {
+			fontSize: '2rem',
+		},
+
+		'&.show': {
+			transform: 'scale(1)',
+			opacity: '1',
+			zIndex: '20',
+			transition: 'all 0.4s ease-in-out',
+			transitionDelay: '0.2s',
+		}
 	}
 
 });
@@ -79,20 +143,19 @@ class ColorBox extends Component {
 	render() {
 		const { name, color, paletteId, colorId, showFullPalette, classes } = this.props;
 		const { copied } = this.state;
-		// const isDarkColor = chroma(color).luminance() <= 0.085;
-		// const isLightColor = chroma(color).luminance() >= 0.7;
+		
 		return (
 			<CopyToClipboard text={color} onCopy={this.changeCopyState}>
 				<div style={{ backgroundColor: color }} className={classes.ColorBox}>
-					<div style={{ backgroundColor: color }} className={`copy-overlay ${copied && "show"}`} />
+					<div style={{ backgroundColor: color }} className={`${classes.copyOverlay} ${copied && "show"}`} />
 
-					<div className={`copy-msg ${copied && "show"} ${classes.copyText}`}>
+					<div className={`${classes.copyMessage} ${copied && "show"} ${classes.copyText}`}>
 						<h1>Copied!</h1>
 						<p>{color}</p>
 					</div>
 
-					<div className={`copy-content`}>
-						<div className={`box-content`}>
+					<div>
+						<div className={classes.boxContent}>
 							<span className={classes.colorName}>{name}</span>
 						</div>
 						<button className={classes.copyButton}>Copy</button>
