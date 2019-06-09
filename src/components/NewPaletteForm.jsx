55c89c7ccaa76af0f96fea1fd73be71f50fec3df
Tanish Grover <tanigrover@gmail.com>
2019-06-09 19:31:16 +0530

    add persistent drawer and color picker to it

--- src/components/NewPaletteForm.jsx
+++ src/components/NewPaletteForm.jsx
@@ -1,23 +1,24 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/styles";
+import React from "react";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import { ChromePicker } from "react-color";
 
-const drawerWidth = 240;
+const drawerWidth = 400;
+
 const styles = theme => ({
 	root: {
 		display: "flex"
@@ -37,7 +38,8 @@ const styles = theme => ({
 		})
 	},
 	menuButton: {
-		marginRight: theme.spacing(2)
+		marginLeft: 12,
+		marginRight: 20
 	},
 	hide: {
 		display: "none"
@@ -58,7 +60,7 @@ const styles = theme => ({
 	},
 	content: {
 		flexGrow: 1,
-		padding: theme.spacing(3),
+		padding: theme.spacing.unit * 3,
 		transition: theme.transitions.create("margin", {
 			easing: theme.transitions.easing.sharp,
 			duration: theme.transitions.duration.leavingScreen
@@ -74,12 +76,79 @@ const styles = theme => ({
 	}
 });
 
-class NewPaletteForm extends Component {
-	state = {};
+class NewPaletteForm extends React.Component {
+	state = {
+		open: false
+	};
+
+	toggleDrawer = () => {
+		this.setState(prevState => ({ open: !prevState.open }));
+	};
 
 	render() {
-		return <div>hello</div>;
+		const { classes, theme } = this.props;
+		const { open } = this.state;
+
+		return (
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar
+					position="fixed"
+					className={classNames(classes.appBar, {
+						[classes.appBarShift]: open
+					})}
+				>
+					<Toolbar disableGutters={!open}>
+						<IconButton
+							color="inherit"
+							aria-label="Open drawer"
+							onClick={this.toggleDrawer}
+							className={classNames(classes.menuButton, open && classes.hide)}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" color="inherit" noWrap>
+							Create New Color Palette
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					className={classes.drawer}
+					variant="persistent"
+					anchor="left"
+					open={open}
+					classes={{
+						paper: classes.drawerPaper
+					}}
+				>
+					<div className={classes.drawerHeader}>
+						<IconButton onClick={this.toggleDrawer}>
+							<ChevronLeftIcon />
+						</IconButton>
+					</div>
+					<Divider />
+					<Typography variant="h4">Design Your Palette</Typography>
+					<div>
+						<Button variant="contained" color="secondary">
+							Clear Palette
+						</Button>
+						<Button variant="contained" color="primary">
+							Random Color
+						</Button>
+					</div>
+					<ChromePicker />
+					<Button variant="contained" color="primary">Add Color</Button>
+				</Drawer>
+				<main
+					className={classNames(classes.content, {
+						[classes.contentShift]: open
+					})}
+				>
+					<div className={classes.drawerHeader} />
+				</main>
+			</div>
+		);
 	}
 }
 
-export default withStyles(styles)(NewPaletteForm);
+export default withStyles(styles, { withTheme: true })(NewPaletteForm);
